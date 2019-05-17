e7267b7bd779d019dabd7434b93fa5ebc62e2037
WinfredZhuwenfeng <1419657141@qq.com>
2019-05-17 19:45:40 +0800

    'header组件布局基本完成'

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import {
   HeaderWrapper,
-  Logo
+  Logo,
+  Nav,
+  NavItem,
+  NavSearch,
+  Addition,
+  Button
 } from './style';
 
 class Header extends Component {
@@ -9,6 +14,18 @@ class Header extends Component {
     return (
       <HeaderWrapper>
         <Logo />
+        <Nav>
+          <NavItem className="left">首页</NavItem>
+          <NavItem className="left">下载App</NavItem>
+          <NavItem className="right">Aa</NavItem>
+          <NavItem className="right">会员</NavItem>
+          <NavItem className="right">登录</NavItem>
+          <NavSearch></NavSearch>
+        </Nav>
+        <Addition>
+          <Button className="writting">写文章</Button>
+          <Button className="reg">注册</Button>
+        </Addition>
       </HeaderWrapper>
     )
   }
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -5,7 +5,10 @@ export const HeaderWrapper = styled.div`
   position: relative;
   height: 56px;
   border-bottom: 1px solid #f0f0f0;
-`
+  margin: 0 auto;
+  min-width: 768px;
+  max-width: 1440px;
+`;
 
 export const Logo = styled.a.attrs({href: '/'})`
   height: 56px;
@@ -15,4 +18,68 @@ export const Logo = styled.a.attrs({href: '/'})`
   width: 100px;
   background: url(${logoPic});
   background-size: contain;
-`
+`;
+
+export const Nav = styled.div`
+  width: 960px;
+  height: 100%;
+  margin: 0 auto;
+`;
+
+export const NavItem = styled.div`
+  &.left {
+    float: left;
+    color: #333;
+  }
+  &.right {
+    float: right;
+    color: #969696;
+  }
+  &.active {
+    color: #ea6f5a;
+  }
+  line-height: 26px;
+  padding: 15px;
+  font-size: 17px;
+`;
+
+export const NavSearch = styled.input.attrs({
+  placeholder: '搜索'
+})`
+  background-color: #eee;
+  border-radius: 19px;
+  padding: 0 20px;
+  box-sizing: border-box;
+  height: 38px;
+  margin-top: 9px;
+  border: none;
+  outline: none;
+`;
+
+
+export const Addition = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+  height: 100%;
+`;
+
+export const Button = styled.div`
+  font-size: 15px;
+  text-align: center;
+  float: right;
+  margin-top: 9px;
+  line-height: 38px;
+  border-radius: 19px;
+  &.reg {
+    border: 1px solid rgba(236,97,73,.7);
+    color: #ea6f5a;
+  }
+  &.writting {
+    background-color: #ea6f5a;
+    color: #fff;
+  }
+  padding: 0 20px;
+  box-sizing: border-box;
+  margin-left: 20px;
+`;
