e489923c3929c8b90ff381f54aaa33d87f5931b4
WinfredZhuwenfeng <1419657141@qq.com>
2019-05-24 13:46:53 +0800

    '首页组件划分'

---
+++ src/pages/home/styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const HomeWrapper = styled.div` 
+  width: 960px;
+  margin: 0 auto;  
+  overflow: hidden;
+  background-color: yellow;
+`
+export const HomeLeft = styled.div` 
+  width: 625px;
+  float: left;
+  margin-left: 15px;
+  padding-top: 30px;
+  background-color: red;
+  .banner-img {
+    width: 625px;
+    height: 270px;
+  }
+`
+export const HomeRight = styled.div` 
+  width: 270px;
+  float: right;
+  background-color: green;
+`
